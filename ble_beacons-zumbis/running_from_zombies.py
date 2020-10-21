14b9f38e4e953f66cee6fd5b8eb00b7322eefa73
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2020-10-21 16:15:31 -0300

    first c

---
+++ ble_beacons-zumbis/running_from_zombies.py
@@ -0,0 +1,28 @@
+import time
+
+from beacontools import BeaconScanner, EddystoneFilter, \
+    EddystoneUIDFrame, EddystoneTLMFrame, EddystoneURLFrame
+
+
+def read_callback(bt_addr, rssi, packet, additional_info):
+    print(packet, ":", rssi)
+    print(bt_addr)
+    print(additional_info)
+    print("-"*100)
+    time.sleep(10)
+
+
+def read_ble():
+    scanner = BeaconScanner(
+        read_callback,
+        device_filter=EddystoneFilter(namespace="edd1ebeac04e5defa017"),
+        packet_filter=[EddystoneUIDFrame,
+                       EddystoneTLMFrame,
+                       EddystoneURLFrame])
+    scanner.start()
+    time.sleep(5)
+    scanner.stop()
+
+
+for i in range(0, 10):
+    read_ble()
